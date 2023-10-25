3b23d1ede3af0f155d813b7bc9f066b651677c9c
vijaykumar78244 <147585017+vijaykumar78244@users.noreply.github.com>
2023-10-25 15:07:03 +0530

    added image field

--- pages/models.py
+++ pages/models.py
@@ -99,6 +99,8 @@ class CertificationModel(models.Model):
     title = models.CharField(max_length=255)
     status = models.BooleanField(default=False)
     content = models.TextField()
+    image = models.ImageField(upload_to='certifications/', default='default_image.png')    
+
     user = models.ForeignKey(User, 
                         on_delete=models.SET_NULL,
                         related_name='certifications_users_field',
