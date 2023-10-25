86375966c9692efb8a07b3b97c2c69a47c763215
Ankaj <141206680+ankajbyte@users.noreply.github.com>
2023-10-25 10:39:05 +0530

    update urls.py

--- ArecharNutra_API/urls.py
+++ ArecharNutra_API/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('', include('faq.urls')),
