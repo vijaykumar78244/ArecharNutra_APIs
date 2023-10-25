5f834d56f03a949706a09fe0cdaf2cc0a626e561
ankajbyte <ankajbyte@gmail.com>
2023-10-25 06:15:55 +0000

    create migration files and update settings

--- ArecharNutra_API/settings.py
+++ ArecharNutra_API/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xh4(+2b^%)riyoy3_66=dghbc===*@xchxu7fjtq32+dh2d#w7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
-    'arechar_nutra', 
+    'arechar_nutra',
     'faq',
     'rest_framework',
     'ckeditor',
