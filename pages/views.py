50a96995bccdf626ef6a44fc459712fe7dd6d13a
vijaykumar78244 <147585017+vijaykumar78244@users.noreply.github.com>
2023-10-25 14:58:20 +0530

    added content field in views.py

--- pages/views.py
+++ pages/views.py
@@ -91,12 +91,12 @@ class NestedDataView(APIView):
                     },
                     "who_are_we_data": {
                         "title": "Who Are We",
-                        "content": "dfdjfsfs", 
+                        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, maxime ipsa fugiat mollitia amet enim nisi maiores quidem magnam sit voluptatum impedit necessitatibus quae voluptatibus placeat itaque quis praesentium dignissimos.", 
                         "data": WhoAreWeSerializer(who_are_we_data, many=True).data,
                     },
                     "certification_data": {
-                        "title": "Certification Data",
-                        "content": "dfdjfsfs", 
+                        "title": "Certification",
+                        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, maxime ipsa fugiat mollitia amet enim nisi maiores quidem magnam sit voluptatum impedit necessitatibus quae voluptatibus placeat itaque quis praesentium dignissimos.", 
                         "data": CertificationSerializer(certification_data, many=True).data,
                     },
                 }
